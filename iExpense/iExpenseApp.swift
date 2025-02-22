15eef097acb76f0d44d8bb0ef18ff50eb83f4994
bazimR <rishaalmuhammed@gmail.com>
2025-02-23 01:30:49 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Rishal Bazim on 23/02/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
