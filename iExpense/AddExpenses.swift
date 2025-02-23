30b6318646a200aafbd2c2dd93c69eb8cb32aa5f
bazimR <rishaalmuhammed@gmail.com>
2025-02-23 23:34:54 +0530

    added userDefault store to persist data

---
+++ iExpense/AddExpenses.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenses.swift
+//  iExpense
+//
+//  Created by Rishal Bazim on 23/02/25.
+//
+
+import SwiftUI
+
+struct AddExpenses: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenses()
+}
