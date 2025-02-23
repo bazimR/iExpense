df09b7600626f6f153c1d9e9abd6966d47587504
bazimR <rishaalmuhammed@gmail.com>
2025-02-23 23:36:36 +0530

    added persistance using USERDEFAULT

--- iExpense/AddExpenses.swift
+++ iExpense/AddExpenses.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct AddExpenses: View {
+    @Environment(\.dismiss) var dismiss
+    var expense: Expense
+
+    @State private var name: String = ""
+    @State private var type: String = "Personal"
+    @State private var amount: Double = 0.0
+
+    private let typesOfExpense = ["Business", "Personal"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Expense name", text: $name)
+                Picker("Expense type", selection: $type) {
+                    ForEach(typesOfExpense, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField(
+                    "Enter amount",
+                    value: $amount,
+                    format: .currency(
+                        code: Locale.current.currency?.identifier ?? "USD")
+                )
+            }.navigationTitle("Add expense").toolbar {
+                Button("Save") {
+                    let newExpense = ExpenseItem(
+                        name: name,
+                        type: type,
+                        amount: amount
+                    )
+
+                    expense.items.append(newExpense)
+                    dismiss()
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    AddExpenses()
+    AddExpenses(expense: Expense())
 }
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ExpenseItem: Identifiable, Codable {
+    var id: UUID = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+@Observable
+class Expense {
+    var items: [ExpenseItem] = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
+    }
+
+    init() {
+        if let saveItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode(
+                [ExpenseItem].self,
+                from: saveItems
+            ) {
+                items = decodedItems
+            }
+        } else {
+            items = []
+        }
+
+    }
+}
+struct ContentView: View {
+    @State private var expense = Expense()
+    @State private var showAddExpense: Bool = false
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expense.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name).font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text(
+                            item.amount,
+                            format:
+                                .currency(
+                                    code: Locale.current.currency?.identifier
+                                        ?? "USD")
+                        ).font(.title3)
+                    }
+                }.onDelete(perform: removeItems)
+            }.navigationTitle("iExpense").toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showAddExpense = true
+                }
+            }.sheet(isPresented: $showAddExpense) {
+                AddExpenses(expense: expense)
+            }
+        }
+    }
+    func removeItems(at offset: IndexSet) {
+        expense.items.remove(atOffsets: offset)
     }
 }
 
